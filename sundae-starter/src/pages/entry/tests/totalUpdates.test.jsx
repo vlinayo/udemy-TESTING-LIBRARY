1dfd78e1b535c6a7e225867b259d12b79bb1e17b
vlinayo <vero1607@hotmail.com>
2024-09-17 21:21:26 +0200

    toppings quiz challenge ready

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -24,3 +24,33 @@ test("update scoop subtotal when scoops change", async () => {
     await user.type(chocolateInput, "2");
     expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtotal when toppings change", async () => {
+    const user = userEvent.setup();
+    render(<Options optionType="toppings"/>);
+
+    //initial value of totals $0.00
+    const toppingsSubtotal = screen.getByText('Toppings total: $', {exact: false});
+    expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+    //update one option cherries topping to true and check subtotal
+    const cherriesOption = await screen.findByRole('checkbox', {name: /Cherries/i});
+    
+    expect(cherriesOption).not.toBeChecked();
+    await user.click(cherriesOption);
+    expect(cherriesOption).toBeChecked();
+    expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+    //update chocolate scoops to 1 and subtotal must be updated
+    const mmsOption = await screen.findByRole('checkbox', {name: 'M&Ms'});
+    expect(mmsOption).not.toBeChecked();
+    await user.click(mmsOption);
+    expect(mmsOption).toBeChecked();
+    expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+    //remove a topping and update totals
+    await user.click(mmsOption);
+    expect(mmsOption).not.toBeChecked();
+    expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+});
